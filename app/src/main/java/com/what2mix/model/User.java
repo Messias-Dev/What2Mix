8310ad7c55037547a7b253e5f8a7e1ec7286d90d
Messias Nunesmaia <messias.nunesmaia@ucsal.edu.br>
2020-09-21 09:56:30 -0300

    User domain creation

---
+++ app/src/main/java/com/what2mix/model/User.java
@@ -0,0 +1,91 @@
+package com.what2mix.model;
+
+import java.util.Objects;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private List<Recipe> recipes;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password, List<Recipe> recipes) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.recipes = recipes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(List<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getId(), user.getId()) &&
+                Objects.equals(getName(), user.getName()) &&
+                Objects.equals(getEmail(), user.getEmail()) &&
+                Objects.equals(getPassword(), user.getPassword()) &&
+                Objects.equals(getRecipes(), user.getRecipes());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getEmail(), getPassword(), getRecipes());
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", recipes=" + recipes +
+                '}';
+    }
+}
