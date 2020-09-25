93d0c09fa1ec05ba72189b333547eb6360941dcc
Alexandre <alexandre.alves@ucsal.edu.br>
2020-09-25 20:36:19 -0300

    Activity signup

--- app/src/main/java/com/what2mix/activity/SignUpActivity.java
+++ app/src/main/java/com/what2mix/activity/SignUpActivity.java
@@ -3,12 +3,16 @@ package com.what2mix.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.what2mix.R;
+import com.what2mix.business.UserBO;
 import com.what2mix.config.FirebaseConfig;
+import com.what2mix.exception.DataInsufficientException;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -25,6 +29,13 @@ public class SignUpActivity extends AppCompatActivity {
 
         assignElements();
 
+        btSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+
     }
 
     private void assignElements(){
@@ -32,6 +43,20 @@ public class SignUpActivity extends AppCompatActivity {
         etSignUpEmail = findViewById(R.id.etLoginEmail);
         etSignUpPassword = findViewById(R.id.etSignUpPassword);
         btSignUp = findViewById(R.id.btSignUp);
+    }
+
+    private void register(){
+        String name = etSignUpName.getText().toString();
+        String email = etSignUpEmail.getText().toString();
+        String password = etSignUpPassword.getText().toString();
+
+        try {
+            new UserBO().register(name, email, password);
+        } catch (FirebaseAuthException e) {
+            e.printStackTrace();
+        } catch (DataInsufficientException e) {
+            e.printStackTrace();
+        }
     }
 
 }
