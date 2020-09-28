4b3418987c62180195517b5b61b89559d57a96bc
Alexandre <alexandre.alves@ucsal.edu.br>
2020-09-28 00:27:09 -0300

    Login e Registro completos

--- app/src/main/java/com/what2mix/activity/UserActivity.java
+++ app/src/main/java/com/what2mix/activity/UserActivity.java
@@ -35,6 +35,7 @@ public class UserActivity extends AppCompatActivity {
         auth.signOut();
         Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
         startActivity(intent);
+        finish();
     }
 
 }
