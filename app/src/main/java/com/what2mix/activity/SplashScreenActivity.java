fea50f19af0c689cd29d027c1a08d81859edc36a
Messias Nunesmaia <messias.nunesmaia@ucsal.edu.br>
2020-10-25 12:37:17 -0300

    Alteração na cor de status bar

--- app/src/main/java/com/what2mix/activity/SplashScreenActivity.java
+++ app/src/main/java/com/what2mix/activity/SplashScreenActivity.java
@@ -1,8 +1,11 @@
 package com.what2mix.activity;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -10,11 +13,14 @@ import com.what2mix.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         getSupportActionBar().hide();
+
+        getWindow().setStatusBarColor(Color.parseColor("#EF7B45"));
 
         new Handler().postDelayed(new Runnable() {
             @Override
